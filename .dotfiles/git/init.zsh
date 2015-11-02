01b8751debe5e12ca7e2cba61251236c5de8cfc4
Glenn Jorde <glenn.jorde@lundalogik.se>
2015-11-02 09:57:17 +0100

    Always clean up local branches

--- .dotfiles/git/init.zsh
+++ .dotfiles/git/init.zsh
@@ -13,6 +13,7 @@ git config --global color.branch auto
 
 # Command defaults
 git config --global alias.log 'log --topo-order'
+git config --global alias.pull 'pull -p'
 
 # Aliases
 git config --global alias.a   'add'
@@ -30,8 +31,7 @@ git config --global alias.cp  'cherry-pick'
 git config --global alias.cpx 'cherry-pick -x'
 git config --global alias.d   'diff'
 git config --global alias.ds  'diff --staged'
-git config --global alias.f   'fetch'
-git config --global alias.fp  'fetch -p'
+git config --global alias.f   'fetch -p'
 git config --global alias.h   'log --pretty=format:"%C(yellow)%h%Creset %cd | %s%C(yellow)%d%Creset %C(red)(%an, %cr)%Creset" --graph --date=short'
 git config --global alias.hb  '!git h `git merge-base head master`..HEAD'
 git config --global alias.r   'rebase'
