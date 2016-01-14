0a0a48837c5d2a7865ba28aa5ed27e8bbcd9663b
Glenn Jorde <glenn.jorde@lundalogik.se>
2016-01-14 13:05:43 +0100

    Expose variables

--- .dotfiles/zsh/src/checks.zsh
+++ .dotfiles/zsh/src/checks.zsh
@@ -1,13 +1,13 @@
 # 0 is true, all others are false (think exit codes)
 
 if [[ $(uname) =~ linux ]]; then
-  IS_LINUX=0
+  export IS_LINUX=0
 fi
 
 if [[ $(uname) =~ darwin ]]; then
-  IS_MAC=0
+  export IS_MAC=0
 fi
 
 if [[ $(uname) =~ cygwin_nt ]]; then
-  IS_CYGWIN=0
+  export IS_CYGWIN=0
 fi
