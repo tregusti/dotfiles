57be7d473b06bba54cefe5de99bcea2a558a3451
Glenn Jorde <glenn.jorde@lundalogik.se>
2016-04-06 20:23:44 +0200

    Cursor color represents the vi mode

--- .dotfiles/zsh/src/misc_config.zsh
+++ .dotfiles/zsh/src/misc_config.zsh
@@ -3,6 +3,37 @@ export TERM=xterm-256color
 export CLICOLOR=1
 export LSCOLORS=ExFxcxdxbxegedabagacad
 
+# set vim mode in zsh
+# https://reza.jelveh.me/2011/09/18/zsh-tmux-vi-mode-cursor
+bindkey -v
+
+# use cursor as indicator of vi mode
+zle-keymap-select () {
+  if [ $KEYMAP = vicmd ]; then
+    if [[ $TMUX = '' ]]; then
+      echo -ne "\033]12;Red\007"
+    else
+      printf '\033Ptmux;\033\033]12;red\007\033\\'
+    fi
+  else
+    if [[ $TMUX = '' ]]; then
+      echo -ne "\033]12;Grey\007"
+    else
+      printf '\033Ptmux;\033\033]12;grey\007\033\\'
+    fi
+  fi
+}
+zle-line-init () {
+  zle -K viins
+  echo -ne "\033]12;Grey\007"
+}
+zle -N zle-keymap-select
+zle -N zle-line-init
+# 10 ms timeout instead of the default 400
+export KEYTIMEOUT=1
+
+
+
 # Enable color in grep
 # http://www.gnu.org/software/grep/manual/html_node/Environment-Variables.html
 export GREP_COLOR='4;36'
