0466c91fa39a9a60402e22d41d41f84f8c325f4b
Glenn Jorde <glenn@tregusti.com>
2017-03-04 14:18:15 +0100

    Add syntax colors to cli

--- .dotfiles/zsh/src/functions.zsh
+++ .dotfiles/zsh/src/functions.zsh
@@ -1,3 +1,13 @@
 function take() {
   mkdir -p $1 && cd $1
 }
+
+
+function syntax() {
+  if [[ -z `which highlight` ]]; then
+    echo 'Maybe do a "brew install highlight"?'
+    return
+  fi
+
+  highlight -S $1 -O ansi $2
+}
