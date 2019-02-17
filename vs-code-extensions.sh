97935528be1b6b5b0f6bbb6ef3d7bd648c84958a
Glenn Jorde <glenn@tregusti.com>
2019-02-17 19:15:07 +0100

    Add vs code extension installation

---
+++ vs-code-extensions.sh
@@ -0,0 +1,13 @@
+code --install-extension EditorConfig.EditorConfig
+code --install-extension NuclleaR.vscode-extension-auto-import
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension amatiasq.sort-imports
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension esbenp.prettier-vscode
+code --install-extension flowtype.flow-for-vscode
+code --install-extension joelday.docthis
+code --install-extension mrcrowl.hg
+code --install-extension ms-python.python
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension vscodevim.vim
