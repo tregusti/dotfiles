8d809e0159b7d2d4cdf0140cd69fac129bac1356
Glenn Jorde <glenn@tregusti.com>
2019-10-14 13:24:56 +0200

    Correct vscode docker ext
    And remofe flow ext since I do not use it.

--- vs-code-extensions.sh
+++ vs-code-extensions.sh
@@ -1,10 +1,9 @@
 code --install-extension EditorConfig.EditorConfig
 code --install-extension NuclleaR.vscode-extension-auto-import
-code --install-extension PeterJausovec.vscode-docker
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension amatiasq.sort-imports
 code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension esbenp.prettier-vscode
-code --install-extension flowtype.flow-for-vscode
 code --install-extension joelday.docthis
 code --install-extension mechatroner.rainbow-csv
 code --install-extension mrcrowl.hg
