02b862842c32cb218b567a180a8af920cd22b6cd
Glenn Jorde <glenn@tregusti.com>
2026-04-18 13:00:36 +0200

    Add hostname in prompt on "other" machines

--- dots/.spaceshiprc.zsh
+++ dots/.spaceshiprc.zsh
@@ -4,3 +4,7 @@ SPACESHIP_TIME_SHOW=true
 # Display battery warning when battery is below threshold
 SPACESHIP_BATTERY_THRESHOLD=20
 
+if [[ "$(hostname)" != "mb2020.local" ]]; then
+  # This sets host to be always displayed when not running on my macbook.
+  SPACESHIP_HOST_SHOW="always"
+fi
